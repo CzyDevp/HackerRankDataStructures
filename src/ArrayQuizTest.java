24310e603c53b8cc6265b68441b1113592565faa
NJ-Developer <navsingh191@gmail.com>
2018-05-27 15:55:58 -0400

    assertThrows implemented!!

---
+++ src/ArrayQuizTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayQuizTest {
+     //NullPointer error
+    /*@org.junit.jupiter.api.Test
+    void process() {
+        String name = "nav";
+        assertThrows(NullPointerException.class,()->{
+            int size = name.length();
+            assertEquals(4,size);
+        });
+    }*/
+}
