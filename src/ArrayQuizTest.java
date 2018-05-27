5c5d5a824b1eff95eb4cdb6bf5047f542dacd84d
NJ-Developer <navsingh191@gmail.com>
2018-05-27 17:37:20 -0400

    parametrized test done!! CsvSource ValueSource

--- src/ArrayQuizTest.java
+++ src/ArrayQuizTest.java
@@ -1,13 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ArrayQuizTest {
      //NullPointer error
-    /*@org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.Test
+    @DisplayName("Null Error")    //cname will appear as this
     void process() {
-        String name = "nav";
+        String name = null;
         assertThrows(NullPointerException.class,()->{
             int size = name.length();
             assertEquals(4,size);
         });
-    }*/
+    }
+
+    //parametrized test assists with dynamic inputs
+    @ParameterizedTest
+    @ValueSource(strings = { "raceCar", "radar", "able was I ere I saw elba" })
+    void testParams(String string){
+        assertTrue(string.length()>0);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"abc,ABC","'',''"})      //empty string treated as a Null by CSV ('') <- solution
+    @DisplayName("Params Test")
+    void testUpperCase(String word, String capitalized){
+        assertEquals(capitalized,word.toUpperCase());
+    }
+
 }
