7f7ec9763a2cd6a0486f095f7601dbb75a0f5401
NJ-Developer <navsingh191@gmail.com>
2018-07-21 13:31:53 -0400

    inheritance done!!

---
+++ src/Parent.java
@@ -0,0 +1,19 @@
+public class Parent {
+    protected static  int count = 0;
+    public Parent(){
+        count++;
+    }
+    static int getCount(){
+        return count;
+    }
+}
+class Child extends Parent{
+    public Child(){
+     count++;
+    }
+    public static void main(String[] args){
+        System.out.println("Count is: "+ getCount());
+        Child c = new Child();
+        System.out.println("Count is: "+ getCount());
+    }
+}
